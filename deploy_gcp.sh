41ca708ef5b6c34b9e8eaf076d3ee4e3e6579be8
weiqiang <wayne.weiqiang@gmail.com>
2021-11-06 21:11:31 -0700

    improve ingress

--- deploy_gcp.sh
+++ deploy_gcp.sh
@@ -5,9 +5,9 @@ kubectl run --quiet=true -it --rm quakeflow-kafka-client --restart='Never' --ima
     --command -- bash -c "kafka-topics.sh --create --topic phasenet_picks --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092 && kafka-topics.sh --create --topic gmma_events --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092 && kafka-topics.sh --create --topic waveform_raw --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092 && kafka-topics.sh --create --topic phasenet_waveform --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092"
 kubectl run --quiet=true -it --rm quakeflow-kafka-client --restart='Never' --image docker.io/bitnami/kafka:2.7.0-debian-10-r68 --restart=Never \
     --command -- bash -c "kafka-configs.sh --alter --entity-type topics --entity-name phasenet_picks --add-config 'retention.ms=-1' --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092 && kafka-configs.sh --alter --entity-type topics --entity-name gmma_events --add-config 'retention.ms=-1' --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092"
-# # For external access:
+## For external access:
 # helm upgrade quakeflow-kafka bitnami/kafka --set externalAccess.enabled=true,externalAccess.autoDiscovery.enabled=true,rbac.create=true
-# # Check topic configs:
+## Check topic configs:
 # kubectl run --quiet=true -it --rm quakeflow-kafka-client --restart='Never' --image docker.io/bitnami/kafka:2.7.0-debian-10-r68 --restart=Never \
 #     --command -- bash -c "kafka-topics.sh --describe --topics-with-overrides --bootstrap-server quakeflow-kafka.default.svc.cluster.local:9092"
 
@@ -15,7 +15,7 @@ kubectl run --quiet=true -it --rm quakeflow-kafka-client --restart='Never' --ima
 kubectl apply -f quakeflow-gcp.yaml
 
 # Add autoscaling
-kubectl autoscale deployment phasenet-api --cpu-percent=80 --min=1 --max=10
+kubectl autoscale deployment phasenet-api --cpu-percent=50 --min=1 --max=10
 kubectl autoscale deployment gmma-api --cpu-percent=80 --min=1 --max=10
 
 # Expose APIs
@@ -23,3 +23,6 @@ kubectl expose deployment phasenet-api --type=LoadBalancer --name=phasenet-servi
 kubectl expose deployment gmma-api --type=LoadBalancer --name=gmma-service
 # kubectl expose deployment quakeflow-streamlit --type=LoadBalancer --name=streamlit-ui
 kubectl expose deployment quakeflow-ui --type=LoadBalancer --name=quakeflow-ui
+
+# Add MINIO storage
+# helm install quakeflow-minio --set accessKey.password=minio --set secretKey.password=minio123 --set persistence.size=1T  bitnami/minio
